a945a084ac7f6f3670855a0186225bde35a88ec1
Reuben-S-J <reuben.sj2003@gmail.com>
2022-02-20 18:40:13 +0530

    second commit

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,4 +4,4 @@ from .models import Project
 # Create your views here.
 def home (request):
     projects=Project.objects.all()#grabs all the objects from the data base
-    return render (request,'portfolio\home.html',{'projects':projects})
+    return render (request,'portfolio/home.html',{'projects':projects})
